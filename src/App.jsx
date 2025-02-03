90b7f69e8c4d4ac40ade458ba91244b81d5f9d91
노수민 <nosumin29@gmail.com>
2025-02-03 15:57:58 +0900

    feat: useState를 사용하여 투두리스트 만들기

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
-function App() {
+import { useState } from "react";
+import AddTask from "./components/AddTask";
+import TaskList from "./components/TaskList";
 
+function App() {
+  const [tasks, setTasks] = useState(initialTasks);
+  const addTaskHandler = (text) => {
+    setTasks([...tasks, {id: nextId++, text, done:false}])
+  }
+  const handleChangeTask = (task) => {
+    setTasks(tasks.map(e => {
+      if(e.id === task.id){
+        return {
+            ...e,
+          text: task.text
+        }
+      }
+      return e;
+    }))
+  }
+  const handleDeleteTask = (id) => {
+    setTasks(tasks.filter(e => e.id !== id))
+  }
   return (
     <>
+      <h2>My TodoList-useReducer</h2>
+      <AddTask addTaskHandler={addTaskHandler}/>
+      <TaskList tasks={tasks} onChangeTask={handleChangeTask} onDeleteTask={handleDeleteTask}/>
     </>
   )
 }
-
+let nextId = 3;
+const initialTasks = [
+  { id: 0, text: 'Visit Kafka Museum', done: true },
+  { id: 1, text: 'Watch a puppet show', done: false },
+  { id: 2, text: 'Lennon Wall pic', done: false },
+]
 export default App
